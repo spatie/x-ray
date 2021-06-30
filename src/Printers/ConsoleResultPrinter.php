faeb86b8283bbe13ba39b6e6661d6752553d2bdd
Patrick Organ <patrick@permafrost.dev>
2021-06-30 16:29:08 -0400

    wip

--- src/Printers/ConsoleResultPrinter.php
+++ src/Printers/ConsoleResultPrinter.php
@@ -3,15 +3,20 @@
 namespace Permafrost\RayScan\Printers;
 
 use Permafrost\RayScan\Results\ScanResult;
+use Symfony\Component\Console\Formatter\OutputFormatterStyle;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class ConsoleResultPrinter extends ResultPrinter
 {
+
+
     /**
      * @param OutputInterface $output
      */
     public function print($output, ScanResult $result, bool $colorize = true)
     {
+        $this->initializeFormatter($output);
+
         $this->printHeader($output, $result);
 
         foreach ($result->snippet->getCode() as $lineNum => $line) {
@@ -33,12 +38,42 @@ class ConsoleResultPrinter extends ResultPrinter
         $output->writeln('');
     }
 
+    protected function initializeFormatter($output): void
+    {
+        $map = [
+            'comment' => ['#334155', null],
+            'keyword' => ['#dd6b20', null],
+            'line-num' => ['#4a5568', null],
+            'method' => ['#ecc94b', null],
+            'str' => ['#38a169', null],
+            'target-line' => [null, '#2d3748'],
+            'line-num-target' => ['#ed64a6', '#2d3748'],
+            'variable' => ['#9f7aea', null],
+            'pointer' => ['#ed64a6', null],
+            'ray-call' => ['#e53e3e', null],
+        ];
+        //fg=#ed64a6;bg=#2d3748
+
+        foreach($map as $name => $colors) {
+            [$fg, $bg] = $colors;
+
+            $outputStyle = new OutputFormatterStyle($fg, $bg);
+            $output->getFormatter()->setStyle($name, $outputStyle);
+
+            // create a "NNN-target" tag as well for highlighting the target line bg
+            if ($bg === null && ! isset($map["{$name}-target"])) {
+                $outputStyle = new OutputFormatterStyle($fg, $map['target-line'][1]);
+                $output->getFormatter()->setStyle("{$name}-target", $outputStyle);
+            }
+        }
+    }
+
     protected function printHeader(OutputInterface $output, ScanResult $result): void
     {
         $output->writeln('');
-        $output->writeln(" Filename: {$result->location->filename}");
+        $output->writeln(" Filename: <href=file://{$result->location->filename}>{$result->location->filename}</>");
         $output->writeln(" Line Num: {$result->location->startLine}");
-        $output->writeln(" Function: {$result->location->name}()");
+        $output->writeln(" Function: <ray-call>{$result->location->name}()</ray-call>");
         $output->writeln(" ------");
     }
 
@@ -55,24 +90,19 @@ class ConsoleResultPrinter extends ResultPrinter
             return $line;
         }
 
+        // use the *-target tag variant to allow bg highlighting of the target line
+        $line = preg_replace('~<([\w-]+)>(.+)</(\1)>~', '<$1-target>$2</$1-target>', $line);
 
-        return "<bg=#2d3748>{$line}</>";
+        return "<target-line>{$line}</target-line>";
     }
 
 
     protected function createOutputLine(string $line, int $currentLineNum, ScanResult $result): string
     {
         $isTargetLine = $currentLineNum === $result->location->startLine;
-        $lineNumColor = $isTargetLine ? '#ed64a6' : '#4a5568';
-        $prefix = $isTargetLine ? ' <fg=#ed64a6;bg=#2d3748>══════❱</>' : '        ';
+        $prefix = $isTargetLine ? ' <pointer>══════❱</pointer>' : '        ';
 
-        $line = sprintf(" [<fg=$lineNumColor>% 4d</>]%-4s%-60s", $currentLineNum, $prefix, $line);
-
-        if ($isTargetLine) {
-            $line = str_replace('<fg=', '<bg=#2d3748;fg=', $line);
-        }
-
-        return $line;
+        return sprintf(" [<line-num>% 4d</line-num>]%-4s%-60s", $currentLineNum, $prefix, $line);
     }
 
     protected function highlightTargetFunction(string $line, int $currentLineNum, ScanResult $result): string
@@ -80,7 +110,8 @@ class ConsoleResultPrinter extends ResultPrinter
         $isTargetLine = $currentLineNum === $result->location->startLine;
 
         if ($isTargetLine) {
-            $line = str_replace($result->location->name . '(', '<fg=#e53e3e>' . $result->location->name . '</>(', $line);
+            //$line = str_replace("{$result->location->name}(", "<ray-call>{$result->location->name}</ray-call>(", $line);
+            $line = preg_replace('~' . $result->location->name . '\s*\(~', "<ray-call>{$result->location->name}</ray-call>(", $line);
         }
 
         return $line;
@@ -89,18 +120,18 @@ class ConsoleResultPrinter extends ResultPrinter
     protected function highlightSyntax(string $line): string
     {
         // comments
-        $line = preg_replace('~(//.*)$~', '<fg=#334155>$1</>', $line);
-        $line = preg_replace('~(/\*\*?.*\*/\s*)$~', '<fg=#334155>$1</>', $line);
+        $line = preg_replace('~(//.*)$~', '<comment>$1</comment>', $line);
+        $line = preg_replace('~(/\*\*?.*\*/\s*)$~', '<comment>$1</comment>', $line);
 
         // variables
-        $line = preg_replace('~(\$\w+)~', '<fg=#9f7aea>$1</>', $line);
+        $line = preg_replace('~(\$\w+)~', '<variable>$1</variable>', $line);
 
         // method calls
-        $line = preg_replace('~(->)(\w+)\s*\(~', '$1<fg=#ecc94b>$2</>(', $line);
+        $line = preg_replace('~(->)(\w+)\s*\(~', '$1<method>$2</method>(', $line);
 
         // strings
-        $line = preg_replace("~('[^']+')~", '<fg=#38a169>$1</>', $line);
-        $line = preg_replace('~("[^"]+")~', '<fg=#38a169>$1</>', $line);
+        $line = preg_replace("~('[^']+')~", '<str>$1</str>', $line);
+        $line = preg_replace('~("[^"]+")~', '<str>$1</str>', $line);
 
         return $line;
     }
@@ -111,7 +142,7 @@ class ConsoleResultPrinter extends ResultPrinter
             'namespace new null PHP_EOL private protected public return self static static string try use void';
 
         foreach(explode(' ', $keywords) as $keyword) {
-            $line = preg_replace('~\b('.$keyword.')\b~', '<fg=#dd6b20>$1</>', $line);
+            $line = preg_replace('~\b('.$keyword.')\b~', '<keyword>$1</keyword>', $line);
         }
 
         return $line;
