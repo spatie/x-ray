47edc0ff38a3720581ab8ac73217c39e2a3e48f2
Patrick Organ <patrick@permafrost.dev>
2021-07-24 19:12:08 -0400

    remove original syntax highlighter classes in favor of v2 syntax highlighter

--- src/Printers/Highlighters/Highlighter.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Permafrost\RayScan\Printers\Highlighters;
-
-interface Highlighter
-{
-    public function highlightLine(string $line, string $targetName, int $currentLineNum, int $targetLineNumber): string;
-}
